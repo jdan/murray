c43ed397c0db4acd95aeb1c403952e3f8bd690be
Jordan Scales <scalesjordan@gmail.com>
2012-07-16 12:24:53 -0400

    fixed indentation

--- murray.js
+++ murray.js
@@ -22,38 +22,39 @@ get_text = function() {
 }
 
 comment = function(text, like_count) {
-    return '<li class="uiUfiComment ufiItem ufiItem">                                                                                \
-        <div class="UIImageBlock clearfix uiUfiActorBlock">                                                                          \
-            <a class="actorPic UIImageBlock_Image UIImageBlock_SMALL_Image" href="" tabindex="-1">                                   \
-                <img class="uiProfilePhoto uiProfilePhotoMedium img" src=' + IMG_SRC + ' alt=""></a>                                 \
-                <div class="commentContent UIImageBlock_Content UIImageBlock_SMALL_Content">                                         \
-                    <a class="actorName" href="http://jordanscales.com">Bill Murray</a>                                              \
-                    <span data-jsid="text" class="commentBody">                                                                      \
-                        ' + text + '                                                                                                 \
-                    </span>                                                                                                          \
-                    <span></span>                                                                                                    \
-                    <div class="commentActions fsm fwn fcg">                                                                         \
-                        <a class="uiLinkSubtle">                                                                                     \
-                            <abbr title="ages ago" data-utime="1337" class="timestamp livetimestamp">                                \
-                                ages ago                                                                                             \
-                            </abbr>                                                                                                  \
-                        </a> ·                                                                                                       \
-                        <span class="fsm fwn fcg">                                                                                   \
-                            <button class="stat_elem as_link cmnt_like_link" type="submit" title="Like this comment">                \
-                                <span class="default_message">                                                                       \
-                                    Like                                                                                             \
-                                </span>                                                                                              \
-                            </button> ·                                                                                              \
-                            <a data-hover="tooltip" data-tooltip-alignh="center" class="comment_like_button"                         \
-                                rel="dialog" href="http://jordanscales.com">                                                         \
-                                <i class="cmt_like_icon"></i>' + like_count + '                                                      \
-                            </a>                                                                                                     \
-                        </span>                                                                                                      \
-                    </div>                                                                                                           \
-                </div>                                                                                                               \
-            </div>                                                                                                                   \
-        </div>                                                                                                                       \
-    </li>';
+    return '<li class="uiUfiComment ufiItem ufiItem">                                                                          \
+        <div class="UIImageBlock clearfix uiUfiActorBlock">                                                                    \
+            <a class="actorPic UIImageBlock_Image UIImageBlock_SMALL_Image"                                                    \
+                href="https://www.facebook.com/pages/Bill-Murray/40950792955" tabindex="-1">                                   \
+                <img class="uiProfilePhoto uiProfilePhotoMedium img" src=' + IMG_SRC + ' alt="">                               \
+            </a>                                                                                                               \
+            <div class="commentContent UIImageBlock_Content UIImageBlock_SMALL_Content">                                       \
+              <a class="actorName" href="https://www.facebook.com/pages/Bill-Murray/40950792955">Bill Murray</a>               \
+              <span data-jsid="text" class="commentBody">                                                                      \
+                  ' + text + '                                                                                                 \
+              </span>                                                                                                          \
+              <span></span>                                                                                                    \
+              <div class="commentActions fsm fwn fcg">                                                                         \
+                  <a class="uiLinkSubtle">                                                                                     \
+                      <abbr title="ages ago" data-utime="1337" class="timestamp livetimestamp">                                \
+                          ages ago                                                                                             \
+                      </abbr>                                                                                                  \
+                  </a> ·                                                                                                       \
+                  <span class="fsm fwn fcg">                                                                                   \
+                      <button class="stat_elem as_link cmnt_like_link" type="submit" title="Like this comment">                \
+                          <span class="default_message">                                                                       \
+                              Like                                                                                             \
+                          </span>                                                                                              \
+                      </button> ·                                                                                              \
+                      <a data-hover="tooltip" data-tooltip-alignh="center" class="comment_like_button"                         \
+                          rel="dialog" href="http://jordanscales.com">                                                         \
+                          <i class="cmt_like_icon"></i>' + like_count + '                                                      \
+                      </a>                                                                                                     \
+                  </span>                                                                                                      \
+              </div>                                                                                                           \
+            </div>                                                                                                             \                                                                                                                \
+        </div>                                                                                                                 \
+   </li>';
 }
 
 add_bill = function() {
